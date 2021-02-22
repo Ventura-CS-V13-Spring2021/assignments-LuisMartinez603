a3ce21338fad0fc397c509fcd49553ba809b8a75
Luis Martinez <luis_martinez34@my.vcccd.edu>
2021-02-22 19:13:48 +0000

    Complete Assignment2-2.cpp

--- Assignment2-2.cpp
+++ Assignment2-2.cpp
@@ -7,11 +7,20 @@ int main()
  cin >> number1 >> number2 >> number3 ;
   
 
- if ( number1 == number2 && number1 == number3 )
-   cout << " n1,n2,n3 are the same " << endl;
- else if ( number1 == number3 )
-   cout << "n1 and n3 are the same" << endl;
+ if ( number1 == number2 )
+{
+  if ( number1 == number3 )
+       cout << "n1,n2,n3 are the same" << endl;
+  else 
+       cout << "n1 and n2 are the same" << endl;
+}
+else 
+{
+  if ( number2 == number3 )
+       cout << "n1,n2,n3 are the same" << endl;
+  else 
+       cout << "n2 and n3 are the same" << endl;
 
-     
-     return 0;
+}
+
 }
