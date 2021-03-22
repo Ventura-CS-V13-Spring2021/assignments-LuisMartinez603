2b2da6212312ce0e06b84e872591be86a5355c14
Luis Martinez <luis_martinez34@my.vcccd.edu>
2021-03-22 05:14:59 +0000

    Complete Assignment2-6.cpp

---
+++ Assignment2-6.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main()
+{
+  int rdnum;
+  int N;
+  ofstream   rdfile;
+
+  rdfile.open("rdnum.txt");
+
+  cout << "Enter the number of rdnum \n";
+  cin >> N;
+
+  srand(time(0));
+  for(int i; i<N; i++)
+  {
+    rdnum = random() % 100;
+    rdfile << rdnum << endl;
+  }
+  rdfile.close();
+
+}
