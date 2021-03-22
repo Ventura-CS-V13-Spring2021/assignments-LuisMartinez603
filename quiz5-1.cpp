e667d7bb53543384175d7185d0dd96c016a2ae39
Luis Martinez <luis_martinez34@my.vcccd.edu>
2021-03-22 23:49:36 +0000

    Editing question 1

---
+++ quiz5-1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main()
+{
+  int rdnum;
+  int N;
+  
+
+  cout << "Enter the number of rdnum \n";
+  cin >> N;
+
+  srand(time(0));
+  for(int i; i<N; i++)
+  {
+    rdnum = random() % 100;
+      rdnum << endl;
+  }
+  .close();
+
+}
