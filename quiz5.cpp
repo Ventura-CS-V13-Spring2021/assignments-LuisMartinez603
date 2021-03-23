d03dc04b5134dafce63c6b8895bba188de5a6a75
Luis Martinez <luis_martinez34@my.vcccd.edu>
2021-03-23 01:52:11 +0000

    worked on question 5

---
+++ quiz5.cpp
@@ -0,0 +1,25 @@
+#include	<iostream>
+using namespace std;
+
+int main()
+{
+	int num,i ;
+  int firstrange, secondrange;
+
+  cout << "Enter 2 ranges : ";
+  cin >> firstrange >> secondrange;
+
+  for(num=firstrange; num <= secondrange; num++)
+  {
+    for(i=2; i<num; i++)
+    {
+      if (num % i == 0)
+       break;
+    }
+    if ( i == num )
+      cout << num << "This number is a prime number\n";
+    else
+      cout << num << "This number is NOT a prime number\n";
+	}
+
+}
